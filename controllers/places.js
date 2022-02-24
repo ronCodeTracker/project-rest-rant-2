6702aaa4fe808a7ae69602c618abe9bc790067b2
Ronald K <2fixelectronics777@gmail.com>
2022-02-24 12:29:06 -0600

    Adding places controller

---
+++ controllers/places.js
@@ -0,0 +1,17 @@
+﻿
+// name:  Ronald Kiefer
+// Hebrew name:   ר ו נ  א ל ד
+// date:  February 24, 2022  Thursday
+// description:  js controller file for rest-rant app
+
+
+const router = require('express').Router()
+
+router.get('/', (req, res) => {
+    res.send('GET /places')
+})
+
+module.exports = router
+
+
+
