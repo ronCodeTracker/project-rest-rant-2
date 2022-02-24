33d3c5be61b57ac6b93c7318d491d9b977aa56d3
Ronald K <2fixelectronics777@gmail.com>
2022-02-24 11:36:14 -0600

    Initital commit

---
+++ index.js
@@ -0,0 +1,18 @@
+﻿
+
+//   Ronald Kiefer
+//    ר ו נ  א ל ד 
+//   February 24, 2022
+//   rest-rant application back end programming
+//   boot camp
+
+
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('Hello world!')
+})
+
+app.listen(3000)
+
