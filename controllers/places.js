87d1cc6c03318466608a0011caae55544f3d7b53
Ronald K <2fixelectronics777@gmail.com>
2022-02-28 13:23:18 -0600

    ready to turn in 3

--- controllers/places.js
+++ controllers/places.js
@@ -6,22 +6,11 @@
 
 
 const router = require('express').Router()
+const places = require("../models/places")
 
 router.get('/', (req, res) => {
 
-    let places = [{
-        name: 'H-Thai-ML',
-        city: 'Seattle',
-        state: 'WA',
-        cuisines: 'Thai, Pan-Asian',
-        pic: 'http://placekitten.com/250/250'
-    }, {
-        name: 'Coding Cat Cafe',
-        city: 'Phoenix',
-        state: 'AZ',
-        cuisines: 'Coffee, Bakery',
-        pic: 'http://placekitten.com/250/250'
-    }]
+    
     //let places = []
 
     res.render('places/index', {places})
--- views/places/index.jsx
+++ views/places/index.jsx
@@ -8,7 +8,7 @@ const Def = require('../default')
 function Index(data) {
     let placesFormatted = data.places.map((place) => {
         return (
-            <div>
+            <div key={place.name}>
                 <h2>{place.name}</h2>
                 <img src={place.pic} alt={place.name}></img>
             </div>
