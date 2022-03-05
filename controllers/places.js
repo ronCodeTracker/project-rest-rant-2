7022d43b9da5d82d37bc7b7ba1639dbe6b6f6763
Ronald K <2fixelectronics777@gmail.com>
2022-03-04 22:10:25 -0600

    Added the edit functionality

--- controllers/places.js
+++ controllers/places.js
@@ -56,6 +56,29 @@ router.get('/:id', (req, res) => {
 
 router.get('/:id/edit', (req, res) => {
 
+    let idd = Number(req.params.id)
+    if (isNaN(idd)) {
+        res.render('error404')
+    }
+    else if (!places[idd]) {
+        res.render('error404')
+    }
+    else {
+        res.render('places/edit', {
+            place: places[req.params.id],
+            index: req.params.id
+        })
+        console.log('id' + req.params.id)
+    }
+
+})//page for editing a place
+
+
+//router.put?  update a place  (places/:id)
+router.put('/:id', (req, res) => {
+
+
+    console.log(Number(req.params.id))
     let id = Number(req.params.id)
     if (isNaN(id)) {
         res.render('error404')
@@ -64,15 +87,25 @@ router.get('/:id/edit', (req, res) => {
         res.render('error404')
     }
     else {
-        res.render('places/edit', { place: places[id] })
+        // Dig into req.body and make sure data is valid
+        if (!req.body.pic) {
+            // Default image if one is not provided
+            req.body.pic = 'http://placekitten.com/400/400'
+        }
+        if (!req.body.city) {
+            req.body.city = 'Anytown'
+        }
+        if (!req.body.state) {
+            req.body.state = 'USA'
+        }
+
+        // Save the new data into places[id]
+        places[id] = req.body
+        res.redirect(`/places/${id}`)
     }
 
-})//page for editing a place
+    
 
-
-//router.put?  update a place  (places/:id)
-router.put('/:id', (req, res) => {
-    res.send('update')
 })
 
 //router.delete delete a place (places/:id)
--- views/places/edit.jsx
+++ views/places/edit.jsx
@@ -5,38 +5,40 @@
 const React = require('react')
 const Def = require('../default.jsx')
 
-function edit_form(data) {
+function edit_form({ place, index }) {
     return (
         <Def>
             <main>
                 <h1>Edit Place</h1>
-                <form method="POST" action={`/places/${data.id}?_method=PUT`}>
+                {   
+                    console.log('index' + index)}
+                <form method="POST" action={`/places/${index}?_method=PUT`}>
                     <div className="form-group">
                         <label htmlFor="name">Place Name</label>
                         <input className="form-control" id="name" name="name"
-                            defaultValue={data.place.name} required />
+                            defaultValue={place.name} required />
                     </div>
                     <div className="form-group">
                         <label htmlFor="pic">Place Picture</label>
                         <input className="form-control" id="pic" name="pic"
-                            defaultValue={data.place.pic} />
+                            defaultValue={place.pic} />
                     </div>
                     <div className="form-group">
                         <label htmlFor="city">City</label>
                         <input className="form-control" id="city" name="city"
-                            defaultValue={data.place.city} />
+                            defaultValue={place.city} />
                     </div>
                     <div className="form-group">
                         <label htmlFor="state">State</label>
                         <input className="form-control" id="state" name="state"
-                            defaultValue={data.place.state} />
+                            defaultValue={place.state} />
                     </div>
                     <div className="form-group">
                         <label htmlFor="cuisines">Cuisines</label>
                         <input className="form-control" id="cuisines" name="cuisines" required
-                            defaultValue={data.place.cuisines} />
+                            defaultValue={place.cuisines} />
                     </div>
-                    <input className="btn btn-primary" type="submit" value="Add Place" />
+                    <input className="btn btn-primary" type="submit" value="Update Place" />
                 </form>
             </main>
         </Def>
